c85f6eab695a8500bbfc5fcd6d3368a32fd0a252
eltammie <lennminh@gmail.com>
2024-08-20 13:11:34 +0700

    set up eslint and prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>vroom vroom</Text>
+      <Text>vroom!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
